3312796822872c0f61e597ba40120817afc80964
Joel <zij22syh@rptu.de>
2025-05-06 09:36:22 +0200

    advances 6.05 test 2 and 3 compile but tests do not work

--- 03_multi-cycle_RISC-V_core/src/test/scala/MultiCycleRISCV32I_tb.scala
+++ 03_multi-cycle_RISC-V_core/src/test/scala/MultiCycleRISCV32I_tb.scala
@@ -8,20 +8,33 @@ package MultiCycleRV32I_Tester
 
 import chisel3._
 import chiseltest._
-import MultiCycleRV32I._
+import core_tile._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class MultiCycleRISCV32ITest extends AnyFlatSpec with ChiselScalatestTester {
+  "MultiCycleRV32I_Tester" should "execute the given binary instructions correctly" in {
+    test(new MultiCycleRV32Icore("src/test/programs/BinaryFile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.clock.setTimeout(100)
 
-"MultiCycleRV32I_Tester" should "work" in {
-    test(new MultiCycleRV32I("src/test/programs/BinaryFile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      val expectedResults = Seq(
+        5,               // ADDI x1, x0, 5      => x1 = 5
+//        10,              // ADDI x2, x0, 10     => x2 = 10
+//        15,              // ADD x3, x1, x2      => x3 = x1 + x2 = 5 + 10
+//        -5,     // SUB x3, x1, x2      => x3 = x1 - x2 = -5 (signed)
+//        15,              // XOR x3, x1, x2      => 0b0101 ^ 0b1010 = 0b1111 = 15
+//        15,              // OR x3, x1, x2       => 0b0101 | 0b1010 = 0b1111 = 15
+//        0,               // AND x3, x1, x2      => 0b0101 & 0b1010 = 0
+//        1,               // SLT x3, x1, x2      => 5 < 10 = 1 (signed)
+//        1,               // SLTU x3, x1, x2     => 5 < 10 = 1 (unsigned)
+//        5120,            // SLL x3, x1, x2      => 5 << (10 & 0x1F) = 5 << 10
+//        0,               // SRL x3, x1, x2      => 5 >> 10 = 0 (logical)
+//        0                // SRA x3, x1, x2      => 5 >>> 10 = 0 (arithmetic)
+      )
 
-        /* 
-         * TODO: Insert testcases from assignment 2 and adapt them for the multi-cycle core
-         */
-           
+      for (expected <- expectedResults) {
+        dut.clock.step(5) // enough to complete all stages
+        dut.io.check_res.expect(expected.U)
+      }
     }
   }
 }
-
-

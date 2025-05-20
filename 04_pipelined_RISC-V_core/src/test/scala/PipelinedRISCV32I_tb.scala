b1d292561e4e8021f4cfe531c3d787368f163341
Joel <zij22syh@rptu.de>
2025-05-20 15:21:22 +0200

    #

--- 04_pipelined_RISC-V_core/src/test/scala/PipelinedRISCV32I_tb.scala
+++ 04_pipelined_RISC-V_core/src/test/scala/PipelinedRISCV32I_tb.scala
@@ -16,9 +16,37 @@ class PipelinedRISCV32ITest extends AnyFlatSpec with ChiselScalatestTester {
 "PipelinedRV32I_Tester" should "work" in {
     test(new PipelinedRV32I("src/test/programs/BinaryFile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
-        /* 
-         * TODO: Insert your testcases from the previous assignments and adapt them for the pipelined core
-         */
+      //dut.clock.setTimeout(100)
+
+      val expectedResults = Seq(
+        5,               // ADDI x1, x0, 5      => x1 = 5
+//        10,              // ADDI x2, x0, 10     => x2 = 10
+//        15,              // ADD x3, x1, x2      => x3 = x1 + x2 = 5 + 10
+//        -5,              // SUB x3, x1, x2      => x3 = x1 - x2 = -5 (signed)
+//        0,               // AND x3, x1, x2      => 0b0101 & 0b1010 = 0
+//        1,               // SLT x3, x1, x2      => 5 < 10 = 1 (signed)
+//        1,               // SLTU x3, x1, x2     => 5 < 10 = 1 (unsigned)
+//        5120,            // SLL x3, x1, x2      => 5 << (10 & 0x1F) = 5 << 10
+//        0,               // SRL x3, x1, x2      => 5 >> 10 = 0 (logical)
+//        0,                // SRA x3, x1, x2      => 5 >>> 10 = 0 (arithmetic)
+//        15,              // XOR x3, x1, x2      => 0b0101 ^ 0b1010 = 0b1111 = 15
+//        15              // OR x3, x1, x2       => 0b0101 | 0b1010 = 0b1111 = 15
+      )
+      var first = true
+      for (expected <- expectedResults) {
+        if (first) {
+          dut.clock.step(4)
+          first = false
+        } else {
+          dut.clock.step(5)
+        }
+        dut.io.check_res.expect(expected.S)
+      }
+
+    }
+}
+}
+
            
     }
   }

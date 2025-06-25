0868ade0267e2997b401b8db94e64c03491f0bf1
Joel <zij22syh@rptu.de>
2025-06-25 17:21:15 +0200

    task 6

---
+++ 06_Branch_Target_Buffer/src/test/scala/BTB_Test.scala
@@ -0,0 +1,13 @@
+
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class BTBTester extends AnyFlatSpec with ChiselScalatestTester {
+
+"HazardDetectionRV32I_Tester" should "work" in {
+    test(new BTB) { dut =>
+
+
+
+
